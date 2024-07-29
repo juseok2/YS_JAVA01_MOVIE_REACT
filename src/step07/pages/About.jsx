2246a9fdce79dff57f9d2622e463047aa59f8702
juseok2 <wntjrwnstjr@naver.com>
2024-07-29 15:56:53 +0900

    step07 영화목록 스타일

---
+++ src/step07/pages/About.jsx
@@ -0,0 +1,9 @@
+import './About.css';
+export function About(){
+  return(
+    <div className='about_container'>
+      <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt eaque maxime beatae neque, accusantium dolore, iusto tempore sit expedita doloremque, molestias culpa. Voluptatem, dolorem. Voluptatum facere sint distinctio debitis necessitatibus.</span>
+      <span>영화리뷰사이트, 2024</span>
+    </div>
+  );
+}
